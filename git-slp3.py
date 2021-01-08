bbab41ad272e1e952f344bf137bff9f430668a14
sapun-thunchanok <71058434+sapun-thunchanok@users.noreply.github.com>
2021-01-08 12:00:22 +0700

    Update git-slp3.py

--- git-slp3.py
+++ git-slp3.py
@@ -172,8 +172,11 @@ def bk_slp():
     return a_s1_tp,a_s1_ta,a_s3_tp,a_s3_ta,b_s1_tp,b_s1_ta,b_s3_tp,b_s3_ta
 def hb_slp():
     API_HOST = 'https://www.huobi.co.th/api'
-    API_KEY = 'c7c22f11-feb097ef-a6aa3e21-ur2fg6h2gf'
-    API_SECRET = b'298d9c50-bc6f69c5-2942dd26-d94df'
+#     API_KEY = 'c7c22f11-feb097ef-a6aa3e21-ur2fg6h2gf'
+    API_KEY = 'f2137555-3a7561ea-bvrge3rf7j-40f17'
+    API_SECRET = b'e0d65096-a2b9419f-7d6e8457-1eca9'
+#     API_SECRET = b'298d9c50-bc6f69c5-2942dd26-d94df'
+
     def json_encode(data):
         return json.dumps(data, separators=(',', ':'), sort_keys=True)
     # check server time
